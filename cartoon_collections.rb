87347282dce0dfe9de83446b46cd4aa0ecd5cda7
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-16 02:19:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ end
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find{|cheddar| "cheddar"}
-  ingredients = ["garlic", "rosemary", "bread"]
+  ingredients = []
   ingredients.find{|gouda| "gouda"}
 end
