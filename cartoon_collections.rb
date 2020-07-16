2f28609aa7fa9feca75cdc429e9d534509b63616
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-16 02:31:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def long_planeteer_calls(assorted_words)
   end
 end
 
-def find_the_cheese(cheese_types)
-  cheese = ["cheddar", "gouda", "camembert"]
-  cheese_types.find{|cheddar| "cheddar"}
+def find_the_cheese(cheese)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find{|cheddar| "cheddar"}
  
 end
