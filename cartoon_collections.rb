9ad75af60b2ddba176dabfe6d7bf5aa2290cb608
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-16 02:46:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,6 @@ end
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.find{cheese_types.include?(word)}
+  cheese.find{|word| word}
+  cheese_types.include?(word)
 end
